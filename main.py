573cff8a7bcf215cc739afbc6d3fafce4ffc6027
Hùng Cường <cuongdayne17@gmail.com>
2025-05-26 18:55:18 +0700

    [update] updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# pyright: reportIndexIssue=false, reportArgumentType=false
+# pyright: reportIndexIssue=false, reportArgumentType=false, reportAttributeAccessIssue=false
 
 import logging
 import MetaTrader5 as mt5
 
 from src.config import get_config 
 from src.infras import init_mt5, shut_mt5
-from src.chart import get_chart
+from src.chart import Chart
 
 logger = logging.getLogger(__name__)
 
@@ -27,8 +27,13 @@ try:
     )
 
     for symbol in symbols.keys():
-        symbols[symbol] = get_chart(symbol, mt5.TIMEFRAME_M12)
+        chart = Chart(symbol, mt5.TIMEFRAME_M12)
+        chart.renew_chart()
+        symbols[symbol] = chart.get_chart()
         print(symbols[symbol])
+
+    # lasttick = mt5.symbol_info_tick("BTCUSD_m")
+    # print(lasttick)
 
 except Exception as e:
     logging.exception(e)
