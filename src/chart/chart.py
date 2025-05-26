dff92d0fa68faea18e9ce66ec2eb702ac1b387db
Hùng Cường <cuongdayne17@gmail.com>
2025-05-26 18:44:19 +0700

    [update] updated chart.py to remove some unwanted functions

--- src/chart/chart.py
+++ src/chart/chart.py
@@ -6,29 +6,52 @@ import MetaTrader5 as mt5
 
 logger = logging.getLogger(__name__)
 
-def get_chart(symbol, timeframe):
-    """
-    Give prices of symbol and timeframe if found
+class Chart:
+    last_time = 0
+    prices = []
 
-    :param symbol: symbol to get
-    :param timeframe: timeframe to get
-    :return array: arrays of mt5's price objects
-    """
-    if not mt5.initialize():
-        logger.exception("Please first establish connection to Meta Trader")
-        return
+    def __init__(self, symbol, time_frame) -> None:
+        self.symbol = symbol
+        self.time_frame = time_frame
 
-    rates = mt5.copy_rates_from_pos(
-        symbol, timeframe,
-        0, 100
-    )
+    def get_chart(self):
+        """
+        Give prices of symbol and timeframe if they are found
 
-    if len(rates) == 0:
-        print("Something has gone wrong...")
-        return None
+        :return array: arrays of mt5's price objects
+        """
+        return self.prices
 
-    return rates
+    def _renew_chart(self):
+        """
+        Renew chart with new symbol's values
+        """
+        if not mt5.initialize():
+            logger.exception("Please first establish connection to Meta Trader")
+            return
+
+        rates = mt5.copy_rates_from_pos(
+            self.symbol, self.time_frame,
+            0, 100
+        )
+
+        self.prices = rates
+
+        if len(rates) == 0:
+            print("Something has gone wrong...")
+            return None
+
+        self.last_time = rates[-1][0]
+    
+    def renew_chart(self):
+        """
+        Renew chart when new tick comes
+        """
+        last_tick = mt5.symbol_info_tick(self.symbol)
+
+        if last_tick.time != self.last_time:
+            self._renew_chart()
 
 __all__ = (
-    "get_chart",
+    "Chart",
 )
