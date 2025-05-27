80c06c88dba77cd13d39e184046483627822804c
Anh Du <anhdungnguyen955@gmail.com>
2025-05-27 16:02:53 +0700

    [remove] remove indicator in Chart

--- src/chart/chart.py
+++ src/chart/chart.py
@@ -2,7 +2,7 @@
 
 import logging
 import MetaTrader5 as mt5
-from src.indicator.macd import calculate
+# from src.indicator.macd import calculate
 
 
 logger = logging.getLogger(__name__)
@@ -92,12 +92,12 @@ class Chart:
         else:
             self.prices = [new_price] + self.prices[:-1]
             
-    def get_close_prices(self):
-        return [candle["close"] for candle in reversed(self.prices)]
+    # def get_close_prices(self):
+    #     return [candle["close"] for candle in reversed(self.prices)]
 
-    def get_macd(self, fast=5, slow=10, signal=9):
-        closes = self.get_close_prices()
-        return calculate_macd(closes, fast, slow, signal)
+    # def get_macd(self, fast=5, slow=10, signal=9):
+    #     closes = self.get_close_prices()
+    #     return calculate_macd(closes, fast, slow, signal)
 
 
 __all__ = ("Chart",)
