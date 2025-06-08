a22eed58e8578279eb1762a8aac3937d5fa79e4c
Cường Nè <cuongdayne17@gmail.com>
2025-06-08 10:36:36 +0700

    đánh dấu caluclate của PriceStrategy là abstract

--- pyta/strategy/price/base.py
+++ pyta/strategy/price/base.py
@@ -1,6 +1,8 @@
+from abc import ABC, abstractmethod
 from ...models.price import Price
 
-class PriceStrategy:
+class PriceStrategy(ABC):
+    @abstractmethod
     def calculate(self, candle: Price) -> float:
         raise NotImplementedError()
 
